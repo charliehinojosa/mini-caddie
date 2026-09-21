8e801401c33ccf5d7ec464c0069ea6dd54a83465
Charlie Hinojosa <charlie@sightbox.me>
2026-09-21 16:37:27 -0700

    Concat_1 cut + bbox_decoders with format_conversion3 as stride32 cls

--- compile_hailo_v3.py
+++ compile_hailo_v3.py
@@ -30,7 +30,7 @@ runner.translate_onnx_model(
     '/content/mini_caddie_golf_best.onnx',
     'mini_caddie_golf',
     start_node_names=['images'],
-    end_node_names=['/model.22/dfl/Reshape', '/model.22/Sigmoid'],
+    end_node_names=['/model.22/Concat_1'],
     net_input_shapes={'images': [1, 3, 640, 640]}
 )
 print("ONNX translated!")
@@ -44,6 +44,26 @@ nms_config = {
     "classes": NUM_CLASSES,
     "regression_length": REGRESSION_LENGTH,
     "background_removal": False,
+    "bbox_decoders": [
+        {
+            "name": "mini_caddie_golf/bbox_decoder41",
+            "stride": 8,
+            "reg_layer": "mini_caddie_golf/conv41",
+            "cls_layer": "mini_caddie_golf/conv42"
+        },
+        {
+            "name": "mini_caddie_golf/bbox_decoder52",
+            "stride": 16,
+            "reg_layer": "mini_caddie_golf/conv52",
+            "cls_layer": "mini_caddie_golf/conv53"
+        },
+        {
+            "name": "mini_caddie_golf/bbox_decoder62",
+            "stride": 32,
+            "reg_layer": "mini_caddie_golf/conv54",
+            "cls_layer": "mini_caddie_golf/format_conversion3"
+        }
+    ]
 }
 
 nms_config_path = '/content/mini_caddie_nms_config.json'
